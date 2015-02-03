7586a02069424915f3efa3d212cee6d38fcfa147
Ivan <ivan.rey@inp.co>
2015-02-02 22:52:29 -0500

    Bugfix, si se hace moñona con la última siendo rana o ranita, se muestra rana, luego moñona, luego ganaste

--- js/controllers/NotificacionCtrl.js
+++ js/controllers/NotificacionCtrl.js
@@ -7,7 +7,9 @@ angular.module('Frosch')
 
         var timer = $timeout(function () {
 
-            if (chico.jugadorActual.gano && $state.current.name != 'jugar.chico.principal.ganaste')
+            if (jugador.monona && $state.current.name != 'jugar.chico.principal.monona' && $state.current.name != 'jugar.chico.principal.ganaste')
+                $state.go('jugar.chico.principal.monona');
+            else if (jugador.gano && $state.current.name != 'jugar.chico.principal.ganaste')
                 $state.go('jugar.chico.principal.ganaste');
             else if (chico.termino)
                 $state.go('jugar.chico.principal.termino');
