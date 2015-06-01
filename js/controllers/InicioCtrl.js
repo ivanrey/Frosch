fd0af46fa6d60dd5404be73197dfbf3899a06187
Ivan Rey <ivan.rey@inpltda.com>
2015-06-01 00:19:30 -0500

    Release 1.1.4 Mensaje de Licencia y nueva ver.

--- js/controllers/InicioCtrl.js
+++ js/controllers/InicioCtrl.js
@@ -1,14 +1,36 @@
 angular.module('Frosch')
-    .controller('InicioCtrl', function ($scope, $state, hotkeys) {
+    .controller('InicioCtrl', function ($scope, $state, hotkeys, $interval, config) {
 
         $scope.iniciar = function () {
             $state.go('jugar.seleccionEquipos');
+            $interval.cancel(timeoutVideo);
         };
+
+        $scope.minutosProximoVideo = Math.floor((Math.random() * (config.configuracion.tiempoEntreVideos[1] - config.configuracion.tiempoEntreVideos[0])) + config.configuracion.tiempoEntreVideos[0]);
+        $scope.minutosSinVideo = 0;
+
+        var timeoutVideo = $interval(function () {
+
+            if ($scope.minutosSinVideo >= $scope.minutosProximoVideo) {
+                $state.go('inicio.video');
+            }else{
+                $scope.minutosSinVideo++;
+            }
+
+
+        }, 1000); //corre cada minuto
+
+
 
         hotkeys.bindTo($scope)
             .add({
                 combo: 'enter',
                 description: 'Iniciar',
                 callback: $scope.iniciar
-            })
+            });
+
+        $scope.$on('destroy', function () {
+            $interval.cancel(timeoutVideo);
+        }); //por si se va a otro estado.
+
     });
