e57e75851fd07f466228f07eafd36440622a5caf
Ivan Rey <ivan.rey@inpltda.com>
2015-08-22 16:21:54 -0500

    closes #38 Registro de errores en archivo en el directorio home

--- js/services/errorHandler.js
+++ js/services/errorHandler.js
@@ -1,3 +1,11 @@
-/**
- * Created by ivan on 8/22/15.
- */
+if (typeof require !== 'undefined') {
+    var fs = require('fs');
+    var homeOrTmp = require('home-or-tmp');
+
+    window.onerror = function (errorMsg, url, lineNumber, colNumber) {
+
+        var now = new Date();
+        var mensaje = now.toString() + ' ' + errorMsg + '@' + url + ':' + lineNumber + '.' + colNumber;
+        fs.appendFile(homeOrTmp + '/errores.txt', mensaje);
+    };
+}
